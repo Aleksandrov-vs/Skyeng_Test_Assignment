2139362b61578f9017126da0b63081528e9251c4
vasya <dfcz200115@mail.ru>
2023-08-17 15:04:36 +0600

    feat(postgres): добавил postgres

--- django_app/config/components/database.py
+++ django_app/config/components/database.py
@@ -1,8 +1,17 @@
-from config.settings import BASE_DIR
+import logging
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'OPTIONS': {
+            # Нужно явно указать схемы, с которыми будет работать приложение.
+            'options': '-c search_path=public'
+        }
     }
 }
